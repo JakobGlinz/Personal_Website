e1664672dea8a214a965c1ecc7ce489444057076
Jakob Glinz <jakob.glinz@student.tugraz.at>
2024-03-09 16:22:03 +0100

    Setting up the router and the NavBar component

--- Personal_Website/src/App.jsx
+++ Personal_Website/src/App.jsx
@@ -1,9 +1,26 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
+
+import NavBar from './NavBar.jsx';
+
+
+import './App.css';
+import './index.css';
 
 function App() {
 
   return (
     <>
+    <Router>
+      <Routes>
+        <Route path="/" element={
+                                  <>
+                                    <NavBar />
+                                  </>
+                                } />
+        <Route path="/" element={<></>} />
+      </Routes>
+    </Router>
     </>
   )
 }
